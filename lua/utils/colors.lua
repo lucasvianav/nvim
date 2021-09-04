0eb035b20e1d02d4ed62d2b4e2a3cf9123dab1e9
Lucas Viana <oviana@outlook.com>
2021-09-04 16:01:53 -0300

    Add lua/utils/ dir

---
+++ lua/utils/colors.lua
@@ -0,0 +1,14 @@
+local M = {
+    transparent = '',
+    white       = '#FFFFFF',
+    offWhite    = '#F7F7F7',
+    grey        = '#5a5c68',
+    lightBlue   = '#2496ed',
+    cyan        = '#519aba',
+    lightPink   = '#e535ab',
+    yellow      = '#cbcb41',
+    offBlack    = '#202020',
+    black       = '#0a0a0a',
+}
+
+return M
---
+++ lua/utils/init.lua
@@ -0,0 +1,5 @@
+local M = {
+    colors = require('utils.colors')
+}
+
+return M
