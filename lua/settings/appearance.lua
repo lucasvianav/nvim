60ca4aaf683db37b0eaa6896a5a8f5c9b3b6d794
Lucas Viana <oviana@outlook.com>
2021-09-07 23:42:40 -0300

    Update appearance settings

---
+++ lua/settings/appearance.lua
@@ -0,0 +1,24 @@
+local colors = require('utils').colors
+local cmd = vim.cmd
+
+-- the background color is dark
+vim.o.background = 'dark'
+
+-- only the chosen colorscheme 
+-- will be loaded by packer :)
+colorscheme = 'tokyonight'
+
+-- common config to be run for each plugin
+appearance_cmds = [[
+    hi Folded ctermbg=NONE guibg=NONE
+]]
+
+-- -- disabling background color
+-- cmd([[
+--     hi Normal      ctermbg=NONE guibg=NONE
+--     hi SignColumn  ctermbg=NONE guibg=NONE
+--     hi LineNr      ctermbg=NONE guibg=NONE
+--     hi EndOfBuffer ctermbg=NONE guibg=NONE ctermfg=NONE guifg=NONE
+--     hi NonText     ctermbg=NONE guibg=NONE
+-- ]])
+
