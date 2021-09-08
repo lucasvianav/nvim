d9cd5786a8960c8ad68914b74fbe4925a746f7d9
Lucas Viana <oviana@outlook.com>
2021-09-07 23:48:40 -0300

    Setup AutoSession + Dasboard start screen

--- lua/utils/functions.lua
+++ lua/utils/functions.lua
@@ -1,6 +1,9 @@
 local M = {}
 
-function map(mode, lhs, rhs, opt)
+local fn = vim.fn
+local cmd = vim.cmd
+
+function _G.map(mode, lhs, rhs, opt)
     local available_modes = 'nvsxo!ilct'
     if #mode ~= 1 or not string.find(available_modes, mode) then return end
 
@@ -11,10 +14,28 @@ function map(mode, lhs, rhs, opt)
 end
 M.map = map
 
-function unmap(mode, lhs)
+function _G.unmap(mode, lhs)
     vim.api.nvim_set_keymap(mode, lhs, '<nop>', {})
 end
 M.unmap = unmap
 
+function _G._loadfile(module)
+    return function() return require(module) end
+end
+M.loadfile = _loadfile
+
+function _G.loadSessionOrDashboard()
+    local sessionsDir = fn.expand('~/.local/share/nvim/sessions/')
+    local cwd = fn.expand(fn.getcwd())
+    local session = sessionsDir .. fn.substitute(cwd, '/', '\\%', 'g') .. '.vim'
+    local noArguments = vim.fn.argc()
+
+    if fn.filereadable(session) == 1 then
+        cmd('silent! RestoreSession')
+    elseif noArguments == 0 then
+        cmd('silent! Dashboard')
+    end
+end
+M.loadSessionOrDashBoard = loadSessionOrDashboard
 
 return M
