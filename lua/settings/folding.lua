2765dfe75477f9c9682dbd6a332098d4fadfea8b
Lucas Viana <oviana@outlook.com>
2021-09-07 23:43:04 -0300

    Port folding settings

---
+++ lua/settings/folding.lua
@@ -0,0 +1,21 @@
+local o  = vim.o
+local fn = vim.fn
+
+local treesitterEnabled, ts = pcall(require,'nvim-treesitter.configs')
+
+-- use treesitter for folding
+-- if available, else indent
+if treesitterEnabled then
+    o.foldmethod = 'expr'
+    o.foldexpr   = fn['nvim_treesitter#foldexpr']()
+else
+    o.foldmethod = 'indent'
+end
+
+--[[
+    autocmd VimEnter,VimLeave * bufdo set foldlevel=0
+    autocmd BufAdd,Bufnew <afile> set foldlevel=0
+    " set foldlevelstart=0                " start folded
+endif
+]]--
+
