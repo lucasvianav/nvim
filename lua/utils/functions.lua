f59b064598e619240deab491a101bc48f5b51b6d
Lucas Viana <oviana@outlook.com>
2021-09-09 22:03:23 -0300

    Create wraper function for packer.use + remove module from util.functions

--- lua/utils/functions.lua
+++ lua/utils/functions.lua
@@ -1,5 +1,3 @@
-local M = {}
-
 local fn = vim.fn
 local cmd = vim.cmd
 
@@ -12,17 +10,14 @@ function _G.map(mode, lhs, rhs, opt)
 
     return options
 end
-M.map = map
 
 function _G.unmap(mode, lhs)
     vim.api.nvim_set_keymap(mode, lhs, '<nop>', {})
 end
-M.unmap = unmap
 
 function _G._loadfile(module)
     return function() return require(module) end
 end
-M.loadfile = _loadfile
 
 function _G.loadSessionOrDashboard()
     local sessionsDir = fn.expand('~/.local/share/nvim/sessions/')
@@ -36,6 +31,75 @@ function _G.loadSessionOrDashboard()
         cmd('silent! Dashboard')
     end
 end
-M.loadSessionOrDashBoard = loadSessionOrDashboard
 
-return M
+function _G.getLoadFunction(type, use)
+    local plugin = type == 'plugin'
+    local theme  = type == 'theme'
+    if (not plugin and not theme) or not use then return end
+
+    local function getRequireString(module, pluginName)
+        local requireString = [[    pcall(require, ']] .. module .. [[')]]
+
+        if theme and pluginName then
+            local comm = vim.fn.substitute(appearanceCommands, [[.\+]], '[[&]]', '')
+
+            requireString = requireString .. [[
+            
+                if colorscheme == ']] .. pluginName .. [[' then
+                    vim.cmd('colorscheme ]] .. pluginName .. [[') 
+                    vim.cmd(]] .. comm .. [[)
+                end
+            ]]
+        end
+
+        return requireString
+    end
+
+    local function _use(args, setup)
+        if not args or #args == 0 or not args[1] then return end
+
+        local pluginName = args.as
+
+        if not pluginName then
+            local re = {
+                leadingPath = [[^.\+\/]],           -- matches leading path
+                extension   = [[\.[^.]\+$]],        -- matches trailing extension
+                vim         = [[-n\?vim\|n\?vim-]], -- matches "-vim" or "vim-" (also nvim)
+                lua         = [[-n\?lua\|n\?lua-]], -- matches "-lua" or "lua-"
+            }
+
+            local function subs(regex) 
+                return fn.substitute(pluginName, re[regex], '', 'g')
+            end
+
+            pluginName = args[1]:lower()
+            pluginName = subs('leadingPath')
+            pluginName = subs('extension')
+            pluginName = subs('vim')
+            pluginName = subs('lua')
+        end
+
+        if not args.config or (setup and not args.setup) then
+            local module = 'plugins.' .. type .. 's-config.' .. pluginName
+            local requireString = getRequireString(module, pluginName)
+
+            if setup then
+                args.setup = requireString
+            else
+                args.config = requireString
+            end
+        end
+
+        if theme and pluginName and not args.cond then
+            args.cond = [[colorscheme == ']] .. pluginName .. [[']]
+        end
+
+        -- print(pluginName, type, plugin, theme)
+        -- print(vim.inspect(args))
+
+        return use(args)
+    end
+
+    return _use
+end
+
