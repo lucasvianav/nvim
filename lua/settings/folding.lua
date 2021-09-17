a146557d55d8caf862539c1ee718de6fb18dd99a
Lucas Viana <oviana@outlook.com>
2021-09-16 21:10:16 -0300

    Fold by indent only

--- lua/settings/folding.lua
+++ lua/settings/folding.lua
@@ -1,16 +1,18 @@
-local o  = vim.o
+local o  = vim.opt
 local fn = vim.fn
 
-local treesitterEnabled, ts = pcall(require,'nvim-treesitter.configs')
+-- local treesitterEnabled, ts = pcall(require,'nvim-treesitter.configs')
+-- 
+-- -- use treesitter for folding
+-- -- if available, else indent
+-- if treesitterEnabled then
+--     o.foldmethod = 'expr'
+--     o.foldexpr   = fn['nvim_treesitter#foldexpr']()
+-- else
+--     o.foldmethod = 'indent'
+-- end
 
--- use treesitter for folding
--- if available, else indent
-if treesitterEnabled then
-    o.foldmethod = 'expr'
-    o.foldexpr   = fn['nvim_treesitter#foldexpr']()
-else
-    o.foldmethod = 'indent'
-end
+o.foldmethod = 'indent'
 
 --[[
     autocmd VimEnter,VimLeave * bufdo set foldlevel=0
