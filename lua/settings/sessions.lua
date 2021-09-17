37520390dc27232505c6ba1ff3cadec42e949f12
Lucas Viana <oviana@outlook.com>
2021-09-16 21:04:57 -0300

    Update session options

--- lua/settings/sessions.lua
+++ lua/settings/sessions.lua
@@ -1,3 +1,5 @@
+vim.opt.sessionoptions = 'winsize,resize,buffers,curdir,tabpages,winpos,folds,options'
+
 vim.api.nvim_command([[augroup LoadSessionOrDashboard]])
 vim.api.nvim_command([[au!]])
 vim.api.nvim_command([[au VimEnter * lua load_session_or_dashboard()]])
