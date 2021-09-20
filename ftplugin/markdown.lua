abc34889035ec9aa7b8fcb674dfb7dfff9f05566
Lucas Viana <oviana@outlook.com>
2021-09-20 20:19:12 -0300

    Setup filetype-specific settings

---
+++ ftplugin/markdown.lua
@@ -0,0 +1,6 @@
+local o = vim.opt_local
+
+o.colorcolumn = ''
+o.synmaxcol   = 3000
+o.textwidth   = 0
+o.wrap        = true
---
+++ indent/html.lua
@@ -0,0 +1,5 @@
+local o = vim.opt_local
+
+o.shiftwidth = 2
+o.tabstop    = 2
+
