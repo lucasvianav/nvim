8c6822c91ce23aa2195c9db6ac1156431feca14c
Lucas Viana <oviana@outlook.com>
2021-09-24 16:43:41 -0300

    Change folding to 2+ lines

--- lua/settings/folding.lua
+++ lua/settings/folding.lua
@@ -6,7 +6,7 @@ o.foldmethod = 'indent'
 
 o.fillchars    = [[fold: ]]
 o.foldnestmax  = 4 -- fold 4 nested indent levels at most
-o.foldminlines = 0 -- enable folding 1+ lines
+o.foldminlines = 1 -- enable folding 2+ lines
 
 -- TODO: get indent level more reliably
 -- https://github.com/sindrets/dotfiles/blob/b18533d6f082618233d5178d0e2864987e240a33/.config/nvim/lua/nvim-config/lib.lua#L207-L213
