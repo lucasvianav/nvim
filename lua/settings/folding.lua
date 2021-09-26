65aec42c2189bcc673522f56c0c2b0f346c7120b
Lucas Viana <oviana@outlook.com>
2021-09-26 15:20:14 -0300

    Update foldtext

--- lua/settings/folding.lua
+++ lua/settings/folding.lua
@@ -8,9 +8,7 @@ o.fillchars    = [[fold: ]]
 o.foldnestmax  = 4 -- fold 4 nested indent levels at most
 o.foldminlines = 1 -- enable folding 2+ lines
 
--- TODO: get indent level more reliably
--- https://github.com/sindrets/dotfiles/blob/b18533d6f082618233d5178d0e2864987e240a33/.config/nvim/lua/nvim-config/lib.lua#L207-L213
--- TODO: dispaly first folded line
-o.foldtext = [[substitute(getline(v:foldstart),'\S.\+$','','').'+-- ... (' . (v:foldend - v:foldstart + 1) . ' lines folded) --+']]
+-- TODO: figure out why cindent doesn't work as expected
+o.foldtext = [[substitute(getline(v:foldstart),'\S.\+$','','').'+-- '.substitute(getline(v:foldstart),'^\s\+\(.\{1,40}\).*','\1','').' ... (' . (v:foldend - v:foldstart + 1) . ' lines folded) --+']]
 -- o.foldtext = [[substitute(getline(v:foldstart),'\\t',repeat('\ ',&tabstop),'g').' ... '.trim(getline(v:foldend)) . ' (' . (v:foldend - v:foldstart + 1) . ' lines folded)']]
 
