a88e6a85f45e7d4ca71e5174ee5579805ae32827
Lucas Viana <oviana@outlook.com>
2021-09-26 21:14:43 -0300

    Fix quickscope highlighting

---
+++ after/plugin/autocmds/quickscope.vim
@@ -0,0 +1,2 @@
+hi QuickScopePrimary   gui=underline,bold cterm=underline,bold
+hi QuickScopeSecondary gui=underline,bold cterm=underline,bold
--- lua/_packer/plugins/navigation/quickscope.lua
+++ lua/_packer/plugins/navigation/quickscope.lua
@@ -2,10 +2,14 @@ local function set (property, value)
     vim.g['qs_' .. property] = value
 end
 
-set('highlight_on_keys',  {'f','F','t','T'})          -- trigger a highlight in the appropriate direction
-set('filetype_blacklist', {'startify','dashboard'})   -- don't use plugin in startify/dashboard
-set('max_chars',          240)                        -- dont highlight anything beyond 240 chars
-
-vim.cmd([[hi QuickScopePrimary   gui=underline,bold cterm=underline,bold]])
-vim.cmd([[hi QuickScopeSecondary gui=underline,bold cterm=underline,bold ]])
+set('max_chars',          240) -- dont highlight anything beyond 240 chars
+set('highlight_on_keys',  {    -- trigger a highlight in the appropriate direction
+    'f', 'F',
+    't', 'T',
+})
+set('filetype_blacklist', {    -- don't use plugin in startify/dashboard
+    'startify',
+    'dashboard',
+    'NvimTree',
+})
 
