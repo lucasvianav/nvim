b5fe4bc492f4d8e01fa5645da5e0ae93dbe58c58
Lucas Viana <oviana@outlook.com>
2021-09-27 12:43:52 -0300

    Update Packer bootstrap

--- lua/_packer/init.lua
+++ lua/_packer/init.lua
@@ -2,17 +2,23 @@ local fn = vim.fn
 local cmd = vim.cmd
 
 local loaded, packer = pcall(require, 'packer')
-local packer_path    = fn.stdpath('data') .. '/site/pack/packer/opt/packer.nvim'
-local exists         = (fn.isdirectory(packer_path) == 1)
+local packer_path = fn.stdpath('data') .. '/site/pack/packer/opt/packer.nvim'
+local exists = (fn.isdirectory(packer_path) == 1)
 
 if not loaded and not exists then
     fn.delete(packer_path, 'rf')
     fn.system({
-        'git', 'clone', '--depth', '1',
+        'git',
+        'clone',
+        '--depth',
+        '1',
         'https://github.com/wbthomason/packer.nvim',
         packer_path,
     })
 
+    cmd('packadd packer.nvim')
     loaded, packer = pcall(require, 'packer')
-end
 
+    require('_packer.plugins.general.packer')
+    cmd('PackerSync')
+end
