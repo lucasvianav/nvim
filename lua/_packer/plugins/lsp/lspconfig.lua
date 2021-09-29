331270f1e2f09f330a8a98e0a6077ab54c3076ab
Lucas Viana <oviana@outlook.com>
2021-09-29 10:40:57 -0300

    Fix LSP handlers conf (missing local variable)

--- lua/_packer/plugins/lsp/lspconfig.lua
+++ lua/_packer/plugins/lsp/lspconfig.lua
@@ -3,23 +3,24 @@
 -- TODO: https://www.reddit.com/r/neovim/comments/p0jx12/weird_diagnostic_signs_behavior/h898ft6/?utm_source=reddit&utm_medium=web2x&context=3
 -- TODO: remove this from plugin config
 
+local lsp = vim.lsp
+
 --{{ @HANDLERS
-    lsp.handlers['textDocument/hover'] = lsp.with(lsp.handlers.hover, {
-        border = 'single',
-    })
+lsp.handlers['textDocument/hover'] = lsp.with(lsp.handlers.hover, {
+    border = 'single',
+})
 
-    lsp.handlers['textDocument/signatureHelp'] = lsp.with(lsp.handlers.signature_help, {
-        border = 'single',
-    })
+lsp.handlers['textDocument/signatureHelp'] = lsp.with(lsp.handlers.signature_help, {
+    border = 'single',
+})
 
-    lsp.handlers['textDocument/publishDiagnostics'] = lsp.with(lsp.diagnostic.on_publish_diagnostics, {
-        border           = 'single',
-        virtual_text     = { source = 'always' }, -- or 'if_many'
-        severity_sort    = true,
-        underline        = true,
-        update_in_insert = false,
-    })
+lsp.handlers['textDocument/publishDiagnostics'] = lsp.with(lsp.diagnostic.on_publish_diagnostics, {
+    border = 'single',
+    virtual_text = { source = 'always' }, -- or 'if_many'
+    severity_sort = true,
+    underline = true,
+    update_in_insert = false,
+})
 
-    -- lsp.handlers['textDocument/formatting'] = lsp_formatting_handler,
+-- lsp.handlers['textDocument/formatting'] = lsp_formatting_handler,
 -- @HANDLERS}}
-
