d784a95c23fdd31b8aabb96b0e00840a955c6328
Lucas Viana <oviana@outlook.com>
2021-10-03 11:38:58 -0300

    Remove GutenTags mappings

--- lua/_packer/plugins/lsp/gutentags.lua
+++ lua/_packer/plugins/lsp/gutentags.lua
@@ -2,4 +2,5 @@ local function set(property, value)
     vim.g['gutentags_' .. property] = value
 end
 
-set('cache_dir', vim.fn.expand('$HOME/.cache/tags')) -- don't pollute me project
+set('cache_dir', vim.fn.expand('$HOME/.cache/tags')) -- don't pollute my project
+set('plus_nomap', true) -- don't pollute my mappings
