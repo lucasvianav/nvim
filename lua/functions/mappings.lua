065dddbdaee1bf23a468aefe5f45c5f0786dda1f
Lucas Viana <oviana@outlook.com>
2021-10-04 22:26:05 -0300

    Make map() work with buffer mappings

--- lua/functions/mappings.lua
+++ lua/functions/mappings.lua
@@ -1,25 +1,40 @@
-local set_keymap = vim.api.nvim_set_keymap
-local fn         = vim.fn
+local fn = vim.fn
 
 --[[
 Wrapper for vim.api.nvim_set_keymap(), but defaulting `noremap` and 'silent' options.
-]]--
--- TODO: support buf --- https://github.com/lukas-reineke/dotfiles/blob/5b84e9264d3ca9e40fd773642e5a1d335224733e/vim/lua/utils.lua#L32-L37
-function _G.map(mode, lhs, rhs, opt)
+]]
+function _G.map(modes, lhs, rhs, opts)
+    if type(modes) ~= 'table' then
+        modes = { modes }
+    end
+
+    local options = vim.tbl_extend('force', {
+        noremap = true,
+        silent = true,
+    }, opts or {})
+    local buffer = options.buffer
+    options.buffer = nil
+
     local available_modes = 'nvsxo!ilct'
-    if #mode ~= 1 or not string.find(available_modes, mode) then return end
 
-    local options = vim.tbl_extend('force', { noremap = true, silent = true }, opt or {})
-    set_keymap(mode, lhs, rhs, options)
+    for _, mode in ipairs(modes) do
+        local is_valid_mode = string.find(available_modes, mode)
 
-    return options
+        if #mode == 1 and is_valid_mode then
+            if buffer then
+                vim.api.nvim_buf_set_keymap(0, mode, lhs, rhs, options)
+            else
+                vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+            end
+        end
+    end
 end
 
 --[[
 Wrapper for vim.api.nvim_set_keymap(), mapping the `lhs` to `<nop>`.
-]]--
+]]
 function _G.unmap(mode, lhs)
-    set_keymap(mode, lhs, t'<nop>', {})
+    vim.api.nvim_set_keymap(mode, lhs, t('<nop>'), {})
 end
 
 function _G.CR()
@@ -27,12 +42,12 @@ function _G.CR()
 
     if fn.pumvisible() ~= 0 then
         if fn.complete_info({ 'selected' }).selected == -1 then
-            return has_npairs and ( npairs.esc('<c-e>') .. npairs.autopairs_cr() ) or t'<C-e><CR>'
+            return has_npairs and (npairs.esc('<c-e>') .. npairs.autopairs_cr()) or t('<C-e><CR>')
         else
-            return has_npairs and npairs.esc('<c-y>') or t'<C-y>'
+            return has_npairs and npairs.esc('<c-y>') or t('<C-y>')
         end
     else
-        return has_npairs and npairs.autopairs_cr() or t'<CR>'
+        return has_npairs and npairs.autopairs_cr() or t('<CR>')
     end
 end
 
@@ -40,11 +55,8 @@ function _G.BS()
     local has_npairs, npairs = pcall(require, 'nvim-autopairs')
 
     if fn.pumvisible() ~= 0 and fn.complete_info({ 'mode' }).mode == 'eval' then
-        return has_npairs and ( npairs.esc('<c-e>') .. npairs.autopairs_bs() ) or t'<C-e><BS>'
+        return has_npairs and (npairs.esc('<c-e>') .. npairs.autopairs_bs()) or t('<C-e><BS>')
     else
-        return has_npairs and npairs.autopairs_bs() or t'<BS>'
+        return has_npairs and npairs.autopairs_bs() or t('<BS>')
     end
 end
-
-
-
