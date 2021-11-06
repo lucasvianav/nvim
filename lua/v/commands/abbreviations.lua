bb1f6efda138e9324ccf83736f8891e738223fcd
Lucas Viana <oviana@outlook.com>
2021-11-06 10:54:46 -0300

    Port cabbreviations to Lua

---
+++ lua/v/commands/abbreviations.lua
@@ -0,0 +1,34 @@
+local utils = require('v.utils.abbreviations')
+
+utils.set_abbreviations({
+    -- commands I always get wrong
+    { 'E', 'e' },
+    { 'E!', 'e!' },
+    { 'e1', 'e!' },
+    { 'E1', 'e!' },
+    { 'Q', 'q' },
+    { 'q1', 'q!' },
+
+    -- overwriting
+    { 'W', 'up' },
+    { 'w', 'up' },
+    { 'Wq', 'x' },
+    { 'wq', 'x' },
+
+    -- other commands
+    { 'bufo', 'BufOnly' },
+    { 'qb', 'BufClose' },
+    { 'wqb', 'BqWrite' },
+
+    -- Packer
+    { 'PS ', 'PackerSync' },
+    { 'PC ', 'PackerCompile' },
+    { 'PCl', 'PackerClean' },
+    { 'PSt', 'PackerStatus' },
+
+    -- LSP
+    { 'LI', 'LspInfo' },
+
+    -- other plugins
+    { 'DO', 'DiffviewOpen' },
+}, 'c')
