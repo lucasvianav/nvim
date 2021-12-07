167261bb4633d660deeea87f466f4750c42856f4
Lucas Viana <oviana@outlook.com>
2021-12-07 18:39:15 -0300

    Fix markdown/text setlocal colorcolumn

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,11 +1,10 @@
 local o = vim.opt_local
 
-o.colorcolumn = ''
-o.synmaxcol = 3000
 o.textwidth = 0
+o.synmaxcol = 3000
 o.wrap = true
 
--- TODO: I'm not sure why vim.o.spell = true
+-- TODO: I'm not sure why vim.opt = ...
 -- doesn't work. maybe open an issue?
 vim.api.nvim_command('setlocal spell')
 
--- ftplugin/text.lua
+++ ftplugin/text.lua
@@ -1,6 +1,5 @@
 local o = vim.opt_local
 
-o.colorcolumn = ''
 o.textwidth = 0
 o.wrap = true
 
