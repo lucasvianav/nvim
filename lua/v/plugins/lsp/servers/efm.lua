01700bb281bfd114f1ba493fa8c3a6edc20d29ee
Lucas Viana <oviana@outlook.com>
2022-01-21 12:47:03 -0300

    Update EFM config

--- lua/v/plugins/lsp/servers/efm.lua
+++ lua/v/plugins/lsp/servers/efm.lua
@@ -223,8 +223,8 @@ local M = {
         logLevel = 10,
         logFile = '/tmp/efm.log',
         languages = {
-            javascript = { eslint_d },
-            typescript = { eslint_d },
+            javascript = { eslint_d, prettierd },
+            typescript = { eslint_d, prettierd },
             javascriptreact = { eslint_d, prettierd },
             typescriptreact = { eslint_d, prettierd },
 
