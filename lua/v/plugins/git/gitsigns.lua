e661a53697d8bdea15bced9eaa090316ccc5076c
Lucas Viana <oviana@outlook.com>
2022-02-14 15:47:44 -0300

    Update GitSigns keymap config

--- lua/v/plugins/git/gitsigns.lua
+++ lua/v/plugins/git/gitsigns.lua
@@ -37,32 +37,31 @@ require('gitsigns').setup({
         },
     },
 
-    keymaps = {
-        noremap = true,
-        ['n ]h'] = {
-            expr = true,
-            "&diff ? ']c' : '<cmd>lua require\"gitsigns.actions\".next_hunk()<CR>'",
-        },
-        ['n [h'] = {
-            expr = true,
-            "&diff ? '[c' : '<cmd>lua require\"gitsigns.actions\".prev_hunk()<CR>'",
-        },
-        ['n <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
-        ['v <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
-        ['n <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
-        ['v <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
-        ['n <leader>hu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
-        ['n <leader>hp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
-        ['n <leader>hi'] = '<cmd>lua require"gitsigns".blame_line()<CR>',
-        ['n <leader>hI'] = '<cmd>lua require"gitsigns".blame_line({full=true})<CR>',
-        ['n <leader>hS'] = '<cmd>lua require"gitsigns".stage_buffer()<CR>',
-        ['n <leader>hU'] = '<cmd>lua require"gitsigns".reset_buffer_index()<CR>',
-        ['n <leader>hb'] = '<cmd>lua require("gitsigns").toggle_current_line_blame()<CR>',
+    on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
 
-        -- text objects for git hunks
-        ['o ih'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
-        ['x ih'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
-    },
+        require('v.utils.mappings').set_keybindings({
+            { { 'n', 'v' }, '<leader>hs', ':Gitsigns stage_hunk<CR>' },
+            { { 'n', 'v' }, '<leader>hr', ':Gitsigns reset_hunk<CR>' },
+            { 'n', '<leader>hu', gs.undo_stage_hunk },
+            { 'n', '<leader>hp', gs.preview_hunk },
+            { 'n', '<leader>hi', gs.blame_line },
+            {
+                'n',
+                '<leader>hI',
+                function()
+                    gs.blame_line({ full = true })
+                end,
+            },
+            { 'n', '<leader>hU', gs.reset_buffer_index },
+            { 'n', '<leader>hb', gs.toggle_current_line_blame },
+            { 'n', ']h', "&diff ? ']h' : '<cmd>Gitsigns next_hunk<CR>'", { expr = true } },
+            { 'n', '[h', "&diff ? '[h' : '<cmd>Gitsigns prev_hunk<CR>'", { expr = true } },
+            { { 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>' },
+        }, {
+            buffer = bufnr,
+        })
+    end,
 
     current_line_blame = false,
     current_line_blame_opts = {
