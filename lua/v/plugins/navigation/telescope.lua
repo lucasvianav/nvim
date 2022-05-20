38d9e48c517fed873c1409bb407b59f87098d72f
Lucas Viana <oviana@outlook.com>
2022-05-20 09:48:07 -0300

    Update Telescope file filtering

--- lua/v/plugins/navigation/telescope.lua
+++ lua/v/plugins/navigation/telescope.lua
@@ -61,7 +61,7 @@ telescope.setup({
     find_files = {
       follow = true,
       hidden = true,
-      file_ignore_patterns = { '^.git', '^.git/', '^.git/*' },
+      file_ignore_patterns = { '^.git', '^.git/', '^.git/*', '^./.git' },
     },
 
     git_commits = {
