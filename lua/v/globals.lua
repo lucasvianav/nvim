caaad00361d7e696bd703dec179865398dee9066
Lucas Viana <vianavlucas@gmail.com>
2025-05-20 18:39:24 -0300

    hide plugin deprecation messages

--- lua/v/globals.lua
+++ lua/v/globals.lua
@@ -12,11 +12,23 @@ function _G.P(...)
   return wrappers.inspect(...)
 end
 
-vim.print = _G.P
-
 function _G.D(...)
   return wrappers.dump_text(...)
 end
 
 -- load string globals
 require('v.utils.strings')
+
+-- overwrite global methods
+vim.print = _G.P
+
+-- only show deprecation notice if it's coming from my config
+local __vim_deprecate = vim.deprecate
+vim.deprecate = function(...)
+  local config_path = vim.fn.stdpath('config')
+  local deprecated_path = debug.getinfo(3).source
+
+  if deprecated_path:starts_with(config_path) then
+    return __vim_deprecate(...)
+  end
+end
