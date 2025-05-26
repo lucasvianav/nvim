2de2b67b0fa347724abdb13e19a6b149eeffccf0
Lucas Viana <vianavlucas@gmail.com>
2025-05-26 11:00:43 -0300

    revamp dashboard

--- ftplugin/dashboard.lua
+++ ftplugin/dashboard.lua
@@ -2,7 +2,7 @@ vim.opt_local.foldenable = false
 
 require("v.utils.mappings").map({
   "n",
-  "q",
+  { "q", "<c-c>" },
   function()
     vim.api.nvim_buf_delete(0, { unload = true, force = true })
   end,
--- lua/v/plugins/navigation/dashboard.lua
+++ lua/v/plugins/navigation/dashboard.lua
@@ -12,34 +12,50 @@ require("dashboard").setup({
     header = require("v.utils.ascii").neovim_3,
     center = {
       {
-        icon = pad("", 5),
-        desc = pad("Find File", 10),
-        keymap = "SPC f f",
+        icon = pad("  ", 5),
+        desc = pad("Find File", 35),
+        key = "SPC f f",
+        action = function()
+          require("v.plugins.navigation.telescope.pickers").find_files_fd()
+        end,
       },
       {
-        icon = pad("", 5),
-        desc = pad("Grep", 10),
-        keymap = "SPC f p",
+        icon = pad("󰺮  ", 5),
+        desc = pad("Grep", 35),
+        key = "SPC f p",
+        action = function()
+          require("v.plugins.navigation.telescope.pickers").multi_grep()
+        end,
       },
       {
-        icon = pad("", 5),
-        desc = pad("New File", 10),
-        keymap = "CMD ene",
+        icon = pad("󱓺  ", 5),
+        desc = pad("New File", 35),
+        key = "CMD ene",
+        action = "DashboardNewFile",
       },
       {
-        icon = pad("", 5),
-        desc = pad("Navigate Dotfiles", 10),
-        keymap = "SPC f d",
+        icon = pad("󱁤  ", 5),
+        desc = pad("Navigate Dotfiles", 35),
+        key = "SPC f d",
+        action = function()
+          require("v.plugins.navigation.telescope.searchers").find_dotfiles()
+        end,
       },
       {
-        icon = pad("", 5),
-        desc = pad("Navigate Neovim Settings", 10),
-        keymap = "SPC f n",
+        icon = pad("  ", 5),
+        desc = pad("Navigate Neovim Settings", 35),
+        key = "SPC f n",
+        action = function()
+          require("v.plugins.navigation.telescope.searchers").find_nvim()
+        end,
       },
       {
-        icon = pad("", 5),
-        desc = pad("Exit Dashboard", 10),
-        keymap = "q",
+        icon = pad("󰗼  ", 5),
+        desc = pad("Exit Dashboard", 35),
+        key = "<C-C>/q",
+        action = function()
+          vim.api.nvim_buf_delete(0, { unload = true, force = true })
+        end,
       },
     },
     footer = { "      " },
